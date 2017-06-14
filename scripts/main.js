1f5cffe8bb72ab04da9d475afd9c5c3a62bef3e7
Jeff Bothe <jeffrymbothe@gmail.com>
2017-06-14 11:59:10 -0500

    version 4

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,47 @@
 var todoList = {
-  todos: ["item 1", "item 2", "item 3"],
+
+  todos: [],
+
   displayTodos: function() {
-    console.log(this.todos);
+    if (this.todos.length === 0) {
+      console.log("Your todo list is empty!");
+    } else {
+      console.log("My Todos:");
+      for (i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed===false) {
+          console.log("( ) ", this.todos[i].todoText);
+        } else {
+          console.log("(X) ", this.todos[i].todoText);
+        }
+      }
+    }
   },
-  addTodo: function(todo) {
-    this.todos.push(todo);
+
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false,
+    });
     this.displayTodos();
   },
-  changeTodo: function(position, todo) {
-    this.todos[position] = todo;
+
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
     this.displayTodos();
   },
+
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
     this.displayTodos();
   },
+
+  toggleCompleted: function(position) {
+    // this.todos[position].completed = !this.todos[postion].completed;
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  }
+
 }
 
 // var todos = ["item 1", "item 2", "item 3"];
